ade27a294ac85fff9d701b8cb71be4473e80a40d
Rian Santos <rian.sn31@gmail.com>
2026-04-05 22:40:32 -0300

    init: início do teste

---
+++ js/script.js
@@ -0,0 +1,15 @@
+﻿function doColor() {
+  var canvas = document.getElementById("myCanvas");
+  var colorinput = document.getElementById("colorPicker");
+  var color = colorinput.value;
+  canvas.style.backgroundColor = color;
+}
+function doSquare() {
+  var canvas = document.getElementById("myCanvas");
+  var rangeInput = document.getElementById("rangeInput");
+  var size = rangeInput.value;
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "";
+  ctx.fillRect(50, 50, size, size);
+}
